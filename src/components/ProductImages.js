79dfe0827cb437140193f7e3aeb4b79250ec4ce9
altguerrero <josealberto.guerreroayala@gmail.com>
2021-12-13 08:40:14 -0500

    :bento: Added Product images

---
+++ src/components/ProductImages.js
@@ -0,0 +1,8 @@
+import Store1 from "@images/panos_pizza_1.png";
+import Store2 from "@images/pizzeria_camion_pizza.png";
+import Store3 from "@images/sbarro_pizza.png";
+import Store4 from "@images/stroller_pizza_1.png";
+import Store5 from "@images/trulli_pizza.png";
+import Store6 from "@images/vogliadipizza_pizza.png";
+
+export const ProductImages = [Store1, Store2, Store3, Store4, Store5, Store6];
