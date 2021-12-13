4cbd5a751f16a54c05d72efdaf74869f700ff6a9
altguerrero <josealberto.guerreroayala@gmail.com>
2021-12-13 08:41:26 -0500

    :sparkles: Added product list component

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,8 @@
+import "@styles/ProductList.scss";
+import React from "react";
+
+const ProductList = ({ children }) => {
+  return <section className="ProductList">{children}</section>;
+};
+
+export default ProductList;
